9964a9484eaa58b030df27150d61af0c3f07d674
AndyL86 <alinehan89@outlook.com>
2024-07-28 23:15:14 +0100

    v2 script file

--- assets/js/script-v2.js
+++ assets/js/script-v2.js
@@ -1,25 +1,12 @@
 'use strict';
 
-const cardImg = document.getElementsByClassName("card-select");
-const cardImg2 = document.getElementsByClassName("card-select2");
-const cardImg3 = document.getElementsByClassName("card-select3");
-const cardImg4 = document.getElementsByClassName("card-select4");
-const cardImg5 = document.getElementsByClassName("card-select5");
-const cardImg6 = document.getElementsByClassName("card-select6");
-const licImg = document.getElementsByClassName("lic-select");
-const licImg2 = document.getElementsByClassName("lic-select2");
-const licImg3 = document.getElementsByClassName("lic-select3");
-const licImg4 = document.getElementsByClassName("lic-select4");
-const licImg5 = document.getElementsByClassName("lic-select5");
-const licImg6 = document.getElementsByClassName("lic-select6");
-const userScore = document.getElementsByClassName("user-score");
-const userScore2 = document.getElementsByClassName("user-score2");
-const userScore3 = document.getElementsByClassName("user-score3");
-const userScore4 = document.getElementsByClassName("user-score4");
-const userScore5 = document.getElementsByClassName("user-score5");
-const userScore6 = document.getElementsByClassName("user-score6");
+const cardSelect = document.getElementsByClassName("card-select");
+const startBtn = document.getElementsByClassName("btn-start");
+const gameImg = document.querySelector("#game-img");
+const licSelect = document.getElementsByClassName("lic-select");
 const hideImg = document.getElementsByClassName("img-hide");
-const holdImg = document.getElementsByClassName("card-hold");
+const totalScore = document.getElementsByClassName("total-score");
+const userScore = document.getElementsByClassName("user-score");
 
 const cards = [{
   name: "dice 1",
@@ -65,61 +52,87 @@ const cards = [{
 },
 ];
 
-function startGame() {
-getId();
-const newList = [];
-const imgList = [];
-const scoreList = [];
-for (let i = 0; i < 6; i++) {
-  const imgSelector = Math.trunc(Math.random() * 6);
-  const randomCard = cards.filter((value) => value.image_id === imgSelector);
-  const randImage = randomCard[0].image;
-  const licImg = randomCard[0].licence_img;
-  const cardScore = randomCard[0].score;
-  newList.push(randImage);
-  imgList.push(licImg);
-  scoreList.push(cardScore);
+function randomiser() {
+  const cardList = [];
+  const imgList = [];
+  const scoreList = [];
+  for (let i = 0; i < 6; i++) {
+    const imgSelector = Math.trunc(Math.random() * 6);
+    const randomCard = cards.filter((value) => value.image_id === imgSelector);
+    const cardImage = randomCard[0].image;
+    const licImage = randomCard[0].licence_img;
+    const cardScore = randomCard[0].score;
+    cardList.push(cardImage);
+    imgList.push(licImage);
+    scoreList.push(cardScore);
+  }
+  cardFunction(...cardList);
+  licenceFunction(...imgList);
+  scores(...scoreList);
 }
 
-const cardImage = [...newList];
-const licenceImage = [...imgList];
-const scoreVal = [...scoreList];
-console.log(cardImage, licenceImage, scoreVal);
 
-
-if (holdImg) {
-cardImg[0].src = cardImage[0];
-cardImg2[0].src = cardImage[1];
-cardImg3[0].src = cardImage[2];
-cardImg4[0].src = cardImage[3];
-cardImg5[0].src = cardImage[4];
-cardImg6[0].src = cardImage[5];
-} else {}
-
-
-licImg[0].src = licenceImage[0];
-licImg2[0].src = licenceImage[1];
-licImg3[0].src = licenceImage[2];
-licImg4[0].src = licenceImage[3];
-licImg5[0].src = licenceImage[4];
-licImg6[0].src = licenceImage[5];
-
-userScore[0].innerHTML = scoreVal[0];
-userScore2[0].innerHTML = scoreVal[2];
-userScore3[0].innerHTML = scoreVal[3];
-userScore4[0].innerHTML = scoreVal[4];
-userScore5[0].innerHTML = scoreVal[5];
-userScore6[0].innerHTML = scoreVal[6];
-
-}
-
-function getId() {
-  for (let button of holdImg) {
-    button.addEventListener("click", function() {
-      button.classList.remove("card-hold");
-      button.classList.add("image-border");
-      console.log(button.classList);
-    });
+function cardFunction(a, b, c, d, e, f) {
+  const cardAct = [a, b, c, d, e, f];
+  for (let i = 0; i < 6; i++) {
+    if (cardSelect) {
+      cardSelect[i].src = cardAct[i];
+    }
   }
 }
 
+function licenceFunction(g, h, j, k, l, m) {
+  const licAct = [g, h, j, k, l, m];
+  for (let z = 0; z < 6; z++) {
+    if (cardSelect) {
+      licSelect[z].src = licAct[z];
+    }
+  }
+}
+
+function scores(n, o, p, q, r, s) {
+  const scoreAct = [n, o, p, q, r, s];
+  for (let x = 0; x < 6; x++) {
+    if (cardSelect) {
+      userScore[x].innerHTML = scoreAct[x];
+    }
+  }
+}
+
+function startGame() {
+  randomiser();
+}
+
+/* remove card select class DONT DELETE */
+
+for (let button of cardSelect) {
+  console.log(button);
+  button.addEventListener("click", function() {
+    button.classList.remove("card-select");
+    button.classList.add("image-border");
+    button.classList.add("card-count");
+    // if (e.target.parentNode.querySelector(".lic-select")) {
+    //   e.target.parentNode.querySelector(".lic-select").classList.remove("lic-select");
+    // }
+    // counter();
+  });
+}
+
+
+
+/* list of selected images */
+// function counter() {
+//   const cardCount = [];
+//   const count = document.getElementsByClassName("user-score");
+//   for (var index=0; index < count.length; index++) {
+//     cardCount.push(count[index].textContent);
+//   }
+//   /* when all 6 cards are selected display finished message */
+//   const scoreTaly = [...cardCount];
+//   console.log(scoreTaly);
+//   if (cardCount.length === 6) {
+//     for (let tscore of totalScore) {
+//     tscore.classList.remove("total-score");
+//     }
+//   }
+// }
